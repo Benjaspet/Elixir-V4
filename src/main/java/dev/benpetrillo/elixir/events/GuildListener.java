3391cce327b48c17d31e7e7425b2ef4a9cb3943b
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2022-01-28 17:53:37 -0500

    destroy player & queue on guild remove

---
+++ src/main/java/dev/benpetrillo/elixir/events/GuildListener.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2022 Ben Petrillo. All rights reserved.
+ *
+ * Project licensed under the MIT License: https://www.mit.edu/~amini/LICENSE.md
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * All portions of this software are available for public use, provided that
+ * credit is given to the original author(s).
+ */
+
+package dev.benpetrillo.elixir.events;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import dev.benpetrillo.elixir.managers.ElixirMusicManager;
+import dev.benpetrillo.elixir.managers.GuildMusicManager;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public final class GuildListener extends ListenerAdapter {
+
+    public void onGuildLeave(@NotNull GuildLeaveEvent event) {
+        Guild guild = event.getGuild();
+        final GuildMusicManager musicManager = ElixirMusicManager.getInstance().getMusicManager(event.getGuild());
+        final AudioPlayer audioPlayer = musicManager.audioPlayer;
+        final AudioTrack track = audioPlayer.getPlayingTrack();
+        if (!audioPlayer.isPaused() || !musicManager.scheduler.getQueue().isEmpty()) {
+            musicManager.scheduler.queue.clear();
+            musicManager.audioPlayer.destroy();
+        }
+    }
+}
