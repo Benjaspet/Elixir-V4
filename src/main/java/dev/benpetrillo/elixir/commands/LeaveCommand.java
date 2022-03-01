3b7a69e2479fec4c438406daff63cedd09799756
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2022-03-01 13:56:40 -0500

    added leave command

---
+++ src/main/java/dev/benpetrillo/elixir/commands/LeaveCommand.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2022 Ben Petrillo. All rights reserved.
+ *
+ * Project licensed under the MIT License: https://www.mit.edu/~amini/LICENSE.md
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ * OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * All portions of this software are available for public use, provided that
+ * credit is given to the original author(s).
+ */
+
+package dev.benpetrillo.elixir.commands;
+
+import dev.benpetrillo.elixir.utilities.AudioUtil;
+import dev.benpetrillo.elixir.utilities.DJUtil;
+import dev.benpetrillo.elixir.utilities.EmbedUtil;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.managers.AudioManager;
+import tech.xigam.cch.command.Command;
+import tech.xigam.cch.utils.Interaction;
+
+public final class LeaveCommand extends Command {
+
+    public LeaveCommand() {
+        super("leave", "Leave the current voice channel.");
+    }
+
+    @Override
+    public void execute(Interaction interaction) {
+        final GuildVoiceState selfVoiceState = interaction.getMember().getVoiceState();
+        assert selfVoiceState != null;
+        if (!AudioUtil.audioCheck(interaction)) return;
+        int continueExec; if ((continueExec = DJUtil.continueExecution(interaction.getGuild(), interaction.getMember())) != -1) {
+            interaction.reply(EmbedUtil.sendDefaultEmbed(continueExec + " more people is required to continue.")); return;
+        }
+        final AudioManager audioManager = interaction.getGuild().getAudioManager();
+        if (selfVoiceState.inAudioChannel()) {
+            audioManager.closeAudioConnection();
+        }
+        interaction.reply(EmbedUtil.sendDefaultEmbed("I've left the voice channel."));
+    }
+}
